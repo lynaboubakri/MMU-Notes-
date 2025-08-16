001465e16173c44993096f494ab017030105339c
lynaboubakri <136112777+lynaboubakri@users.noreply.github.com>
2025-08-16 17:11:53 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
 
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const errorBox = document.getElementById("error");
+
+    if (!email.endsWith("@student.mmu.edu.my")) {
+        errorBox.textContent = " Use a valid studnet email";
+        errorBox.style.color = "red";
+        return;
+    }
+    if (password.length < 6) {
+        errorBox.textContent = " Password must be at least 6 characters long";
+        errorBox.style.color = "red";
+        return;
+    }
+    errorBox.textContent = "Login successful !";
+    errorBox.style.color = "green";
+
+    setTimeout(() => {
+        window.location.href = "index1.html";
+    }, 1000)
+});
